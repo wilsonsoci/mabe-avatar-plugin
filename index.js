2474892562ba719337daaed2da03231fe65807ec
Wilson <75645210+wilsonsoci@users.noreply.github.com>
2026-01-05 17:04:04 -0300

    fifth commit

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+
 const { findByProps } = vendetta.metro;
 const { after } = vendetta.patcher;
 
@@ -7,9 +8,10 @@ const CUSTOM_URL = "https://i.imgur.com/hHxK9dY.png";
 
 let unpatch;
 
-export default {
+module.exports = {
     onLoad: () => {
         unpatch = after("getAvatarURL", AvatarModule, (args, res) => {
+            // No mobile, o ID pode vir de formas diferentes
             const userId = args[0]?.id || args[0];
             if (userId === TARGET_ID) return CUSTOM_URL;
             return res;
